0d4ecabdba3a5f20e8d41736ea39750d8f09d0b6
Hannes Hornvall <hornwall.hannes@gmail.com>
2016-01-18 21:58:29 +0100

    Initial commit

---
+++ Corsair Utils/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Corsair_Utils
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumpadColorPicker = new System.Windows.Forms.ColorDialog();
+            this.btnColorPicker = new System.Windows.Forms.Button();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnColorPicker
+            // 
+            this.btnColorPicker.Location = new System.Drawing.Point(137, 39);
+            this.btnColorPicker.Name = "btnColorPicker";
+            this.btnColorPicker.Size = new System.Drawing.Size(110, 41);
+            this.btnColorPicker.TabIndex = 0;
+            this.btnColorPicker.Text = "Pick color";
+            this.btnColorPicker.UseVisualStyleBackColor = true;
+            this.btnColorPicker.Click += new System.EventHandler(this.btnColorPicker_Click);
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.Location = new System.Drawing.Point(12, 12);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(108, 103);
+            this.colorPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 127);
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.btnColorPicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Corsair num lock state";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog NumpadColorPicker;
+        private System.Windows.Forms.Button btnColorPicker;
+        private System.Windows.Forms.Panel colorPanel;
+    }
+}
+
---
+++ Corsair Utils/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CUE.NET.Devices.Keyboard;
+using CUE.NET;
+using CUE.NET.Devices.Keyboard.Keys;
+using CUE.NET.Devices.Keyboard.Enums;
+using CUE.NET.Brushes;
+using System.Threading;
+using CUE.NET.Devices.Keyboard.Extensions;
+using System.Configuration;
+
+namespace Corsair_Utils
+{
+    public partial class Form1 : Form
+    {
+        private CorsairKeyboard keyboard;
+        private RectangleKeyGroup numpadGroup;
+
+        private bool numLockState = Control.IsKeyLocked(Keys.NumLock);
+        private Color selectedColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            selectedColor = (Color)Properties.Settings.Default["KeyPadColor"];
+
+            colorPanel.BackColor = selectedColor;
+            CueSDK.Initialize();
+            keyboard = CueSDK.KeyboardSDK;
+            if (keyboard == null)
+                MessageBox.Show("Keyboard not found!");
+
+            numpadGroup = new RectangleKeyGroup(keyboard, CorsairKeyboardKeyId.NumLock, CorsairKeyboardKeyId.KeypadEnter);
+
+            refreshNumpad();
+
+            var kh = new KeyboardHook(true);
+            kh.KeyUp += Kh_KeyUp;
+        }
+
+        private void refreshNumpad()
+        {
+            if (numLockState)
+            {
+                numpadGroup.Brush = new SolidColorBrush(selectedColor);
+                keyboard.Update();
+            }
+            else
+            {
+                numpadGroup.Brush = new SolidColorBrush(Color.Black);
+                keyboard.Update();
+            }
+        }
+
+        private void Kh_KeyUp(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+            if (key.Equals(Keys.NumLock))
+            {
+                if(numLockState)
+                {
+                    numpadGroup.Brush = new SolidColorBrush(Color.Black);
+                    keyboard.Update();
+                }
+                else
+                {
+                    numpadGroup.Brush = new SolidColorBrush(selectedColor);
+                    keyboard.Update();
+                }
+                numLockState = !numLockState;
+            }
+        }
+
+        private void btnColorPicker_Click(object sender, EventArgs e)
+        {
+            DialogResult colorPickerResult = NumpadColorPicker.ShowDialog();
+            if (colorPickerResult == DialogResult.OK)
+            {
+                colorPanel.BackColor = NumpadColorPicker.Color;
+                selectedColor = NumpadColorPicker.Color;
+                Properties.Settings.Default["KeyPadColor"] = NumpadColorPicker.Color;
+                Properties.Settings.Default.Save();
+                refreshNumpad();
+            }
+        }
+    }
+}
